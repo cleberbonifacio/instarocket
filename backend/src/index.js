6afb9da5a1d2ec0f8f931195ce0fc3b6b456cd37
Cleber Bonifacio <bonifaciocleber@gmail.com>
2020-07-13 15:24:50 -0300

    back ok

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,36 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
+dotenv.config();
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect(
-  "mongodb+srv://semana:semana@cluster0.62g4m.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  "mongodb+srv://`${process.env.DB_USER}`:`${process.env.DB_PASS}`@cluster0.62g4m.mongodb.net/<dbname>?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
 
+app.use((req,res, next) => {
+  req.io = io;
+
+  next();
+});
+
+app.use(cors());
+
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+
 app.use(require("./routes"));
 
-app.listen(3333);
+server.listen(3333);
