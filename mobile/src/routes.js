17c677fcc059df5eba27f3f98e71a2b01f465fad
Cleber Boni <bonifaciocleber@gmail.com>
2020-07-17 17:28:08 -0300

    react navigation

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,19 @@
-import {createAppContainer, createStackNavigator} from 'react-navigation';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import Feed from './pages/Feed';
 import New from './pages/New';
 
-export default createAppContainer(createStackNavigator({Feed, New}));
+export default createAppContainer(
+  createStackNavigator(
+    {
+      Feed,
+      New,
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitle: 'InstaRocket',
+      },
+    },
+  ),
+);
